b2a286cd2d8ff51d26441c66b073aca7c8243395
sahithipala1 <sahithipala1@gmail.com>
2022-01-14 16:30:36 -0500

    First commit of the app to host on heroku.

---
+++ app.py
@@ -0,0 +1,24 @@
+import yfinance as yf
+import streamlit as st
+
+st.write(""" 
+# Simple Stock price App
+
+shown are the stock closing price and volume of google!
+
+""")
+
+ticker_symbol = 'GOOGL'
+
+ticker_Data = yf.Ticker(ticker_symbol)
+
+ticker_Df = ticker_Data.history(period="id", start='2010-5-31', end="2022-5-31")
+
+st.write("""
+
+
+
+""")
+st.line_chart(ticker_Df.Close)
+st.line_chart(ticker_Df.Volume)
+st.line_chart(ticker_Df.Dividends)
